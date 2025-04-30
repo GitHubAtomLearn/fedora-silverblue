bdd73531553a08b2495578eb0d3022ab8934dd6b
GitHubAtomLearn <63038454+GitHubAtomLearn@users.noreply.github.com>
2025-04-30 11:00:00 +0300

    Test rpm-ostree-2025.7-3.fc42

--- fedora-silverblue/install.sh
+++ fedora-silverblue/install.sh
@@ -9,19 +9,22 @@ main() {
     # https://www.mankier.com/1/gpg#Files-GNUPGHOME
     export GNUPGHOME="/tmp/.gnupg"
 
-    dnf --assumeyes upgrade --enablerepo=updates-testing --refresh --advisory=FEDORA-2025-96e470a839
+    # dnf -y upgrade --enablerepo=updates-testing --refresh --advisory=FEDORA-2025-96e470a839
+    
+    dnf install -y https://kojipkgs.fedoraproject.org//packages/rpm-ostree/2025.7/3.fc42/x86_64/rpm-ostree-2025.7-3.fc42.x86_64.rpm \
+        https://kojipkgs.fedoraproject.org//packages/rpm-ostree/2025.7/3.fc42/x86_64/rpm-ostree-libs-2025.7-3.fc42.x86_64.rpm
 
-    dnf --assumeyes remove plymouth plymouth-core-libs plymouth-graphic-libs
-    dnf swap --assumeyes --allowerasing nano vim-default-editor
-    dnf swap --assumeyes --allowerasing noopenh264 mozilla-openh264
+    dnf -y remove plymouth plymouth-core-libs plymouth-graphic-libs
+    dnf swap -y --allowerasing nano vim-default-editor
+    dnf swap -y --allowerasing noopenh264 mozilla-openh264
 
-    dnf --assumeyes install $(grep -Ev '^#|^$' /tmp/packages.txt)
+    dnf -y install $(grep -Ev '^#|^$' /tmp/packages.txt)
 
     systemctl enable virtqemud.socket virtnetworkd.socket virtstoraged.socket \
     rm-mount-for-root-from-etc-fstab.service
 #    systemctl enable libvirtd.socket
 
-    rm --recursive --force /var/lib/unbound/root.key
+    rm -rf /var/lib/unbound/root.key
     chmod ug-s \
         /usr/bin/chage \
         /usr/bin/chfn \
