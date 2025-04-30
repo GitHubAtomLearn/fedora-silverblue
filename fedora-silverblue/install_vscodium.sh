2d82529fea9ceabd0b15489dff3cac34a9ca7c2b
GitHubAtomLearn <63038454+GitHubAtomLearn@users.noreply.github.com>
2025-04-30 11:00:00 +0300

    VSCodium: Use el8 RPMs

--- fedora-silverblue/install_vscodium.sh
+++ fedora-silverblue/install_vscodium.sh
@@ -11,7 +11,10 @@ main() {
 
     latest_releases_url="https://api.github.com/repos/VSCodium/vscodium/releases/latest"
     latest_release="$(curl --location "${latest_releases_url}" | jq --raw-output '.tag_name')"
-    rpm_url="$(curl --location "${latest_releases_url}" | jq --raw-output '.assets[] | select(.name == "codium-'${latest_release}'-el9.'$(uname --machine)'.rpm") | .browser_download_url')"
+    # rpm_url="$(curl --location "${latest_releases_url}" | jq --raw-output '.assets[] | select(.name == "codium-'${latest_release}'-el9.'$(uname --machine)'.rpm") | .browser_download_url')"
+    # https://github.com/VSCodium/vscodium/releases/tag/1.99.32846
+    # https://github.com/VSCodium/vscodium/pull/2350
+    rpm_url="$(curl --location "${latest_releases_url}" | jq --raw-output '.assets[] | select(.name == "codium-'${latest_release}'-el8.'$(uname --machine)'.rpm") | .browser_download_url')"
     curl --location --remote-name-all "${rpm_url}"{,.sha256}
     sha256sum --check ./*.rpm.sha256
     dnf install --assumeyes ./*.rpm
